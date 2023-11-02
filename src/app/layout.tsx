617ef7a1457d359a963af50b1ffb1e9caf5ecf83
Mateus Bezerra <mateusbzerra@gmail.com>
2023-11-02 02:47:35 -0300

    refactor: add tech stack list & changes social buttons & changes short description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Raleway } from "next/font/google";
 const ralewayFont = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mateus Bezerra - Front-end Engineer",
+  title: "Mateus Bezerra - Software Engineer",
   description: "Mateus Bezerra portfolio and blog",
 };
 
@@ -19,11 +19,6 @@ export default function RootLayout({
       <body
         className={`${ralewayFont.className} text-white bg-dark-blue h-full`}
       >
-        <header className="w-full h-28 flex justify-center">
-          <div className="flex w-full max-w-screen-xl px-4 items-center justify-start">
-            <h1 className="text-6xl font-extrabold">M</h1>
-          </div>
-        </header>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,61 @@
 import Image from "next/image";
-import { GithubIcon, MailIcon, LinkedinIcon } from "lucide-react";
+import { TechItem } from "@/components/TechItem/TechItem";
+import { SocialButton } from "@/components/SocialButton/SocialButton";
+import { getTechStackList } from "@/data/techStackList";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+  const yearsOfExperience = currentYear - 2019;
+  const techStackList = getTechStackList({ currentYear });
   return (
-    <main className="flex min-h-fit md:items-center flex-col bg-shape bg-no-repeat bg-cover bg-center md:min-h-screen md:-mt-36 md:bg-shape-full">
+    <main className="flex h-full mb-36 md:mb-0 mt-12 min-h-screen md:items-center flex-col bg-shape bg-no-repeat bg-cover bg-center md:mt-0  md:bg-shape-full">
       <div className="flex flex-1 flex-col-reverse items-center ml-6 mr-6 md:flex-row md:space-x-24 md:justify-center">
         <div className="flex flex-col">
-          <div className="mb-24 md:mb-6">
+          <div className="mb-16 md:mb-12">
             <h1 className="text-7xl md:text-8xl font-extrabold mb-8 md:mb-10">
               Hi :)
             </h1>
-            <h2 className="text-5xl md:text-6xl font-extrabold md:mb-2">
+            <h2 className="text-5xl md:text-6xl font-extrabold mb-6">
               I&apos;m <span className="text-cyan">Mateus Bezerra</span>
             </h2>
-            <h3 className="text-2xl md:text-3xl font-extrabold">
-              Front-end Engineer
-            </h3>
+            <p className="text-xl max-w-md font-normal">
+              {`+${yearsOfExperience}`} years experienced{" "}
+              <span className="text-cyan">Software Engineer</span> with working
+              experience in <b>Front-end</b>, <b>Back-end</b> and <b>Mobile </b>
+              development.
+            </p>
           </div>
-          <div className="flex space-x-5 justify-center mb-12 md:mb-0 md:justify-start">
-            <a
-              target="_blank"
-              href="https://github.com/mateusbzerra"
-              className="hover:text-gray-600"
-            >
-              <GithubIcon size={36} />
-            </a>
-            <a
-              target="_blank"
-              href="mailto:dev@mateusbezerra.com"
-              className="hover:text-gray-600"
-            >
-              <MailIcon size={36} />
-            </a>
-            <a
-              target="_blank"
-              href="https://linkedin.com/in/mateusbzerra"
-              className="hover:text-gray-600 "
-            >
-              <LinkedinIcon size={36} />
-            </a>
+
+          <div>
+            <h1 className="font-bold text-2xl md:mb-3 mb-6">Tech Stack:</h1>
+            <div className="flex flex-wrap gap-3 max-w-xl">
+              {techStackList.map((tech) => (
+                <TechItem key={tech.name} {...tech} />
+              ))}
+            </div>
           </div>
         </div>
-        <Image
-          className="rounded-[8rem] mb-12 md:mb-0"
-          width={400}
-          height={400}
-          alt="Avatar"
-          src="/picofme.png"
-        />
+        <div className="mb-12 md:mb-0">
+          <Image
+            className="mb-6 md:w-96 lg:h-96 w-64 h-64"
+            width={400}
+            height={400}
+            alt="Avatar"
+            src="/picofme.png"
+          />
+          <div className="flex flex-row justify-center gap-4">
+            <SocialButton
+              label="Github"
+              color="#151515"
+              url="https://github.com/mateusbzerra"
+            />
+            <SocialButton
+              label="LinkedIn"
+              color="#0E76A8"
+              url="https://linkedin.com/in/mateusbzerra"
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
